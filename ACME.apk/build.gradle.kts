d8aa76800ac27c023352da77ce5aeac45aed194c
Sebastian Nielsen <sebastian@sebbe.eu>
2026-04-28 10:42:58 +0200

    Update build.gradle.kts
    Changed imports to lite versions to avoid importing QPC libraries.

--- ACME.apk/build.gradle.kts
+++ ACME.apk/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -36,25 +37,19 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.foundation)
-    implementation(libs.bouncycastle.bcpkix)
-    implementation(libs.bouncycastle.bcprov)
-    implementation(libs.nimbus.jose.jwt)
     implementation(libs.retrofit)
-    implementation(libs.retrofit2.kotlinx.serialization.converter)
     implementation(libs.okhttp)
+    implementation(libs.nimbus.jose.jwt)
+    implementation(libs.bouncycastle.bcprov)
+    implementation(libs.bouncycastle.bcpkix)
     implementation(libs.kotlinx.serialization.json)
-    implementation(libs.lifecycle.viewmodel.ktx)
-    implementation(libs.lifecycle.viewmodel.compose)
-    implementation(libs.converter.gson)
-    debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.retrofit.converter.kotlinx)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    compileOnly(libs.error.prone.annotations)
 }
