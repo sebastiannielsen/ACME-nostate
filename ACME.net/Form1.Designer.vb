4dcff1c62efb9bae418dab6be2474573169d3363
Sebastian Nielsen <sebastian@sebbe.eu>
2026-05-14 22:52:25 +0200

    Added HTTP-01 support
    Added HTTP-01 support

--- ACME.net/Form1.Designer.vb
+++ ACME.net/Form1.Designer.vb
@@ -1,4 +1,4 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
@@ -36,6 +36,8 @@ Partial Class Form1
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -47,7 +49,7 @@ Partial Class Form1
         Me.TextBox1.Location = New System.Drawing.Point(164, 6)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9679)
-        Me.TextBox1.Size = New System.Drawing.Size(721, 38)
+        Me.TextBox1.Size = New System.Drawing.Size(806, 38)
         Me.TextBox1.TabIndex = 0
         '
         'TextBox2
@@ -57,7 +59,7 @@ Partial Class Form1
         Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBox2.Location = New System.Drawing.Point(164, 46)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(721, 38)
+        Me.TextBox2.Size = New System.Drawing.Size(806, 38)
         Me.TextBox2.TabIndex = 1
         '
         'TextBox3
@@ -65,10 +67,10 @@ Partial Class Form1
         Me.TextBox3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox3.Font = New System.Drawing.Font("Courier New", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox3.Location = New System.Drawing.Point(8, 182)
+        Me.TextBox3.Location = New System.Drawing.Point(18, 224)
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.ReadOnly = True
-        Me.TextBox3.Size = New System.Drawing.Size(873, 27)
+        Me.TextBox3.Size = New System.Drawing.Size(958, 27)
         Me.TextBox3.TabIndex = 2
         '
         'TextBox4
@@ -77,11 +79,12 @@ Partial Class Form1
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox4.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox4.Location = New System.Drawing.Point(12, 247)
+        Me.TextBox4.Location = New System.Drawing.Point(15, 294)
         Me.TextBox4.Multiline = True
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.ReadOnly = True
-        Me.TextBox4.Size = New System.Drawing.Size(873, 394)
+        Me.TextBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox4.Size = New System.Drawing.Size(958, 452)
         Me.TextBox4.TabIndex = 3
         '
         'Button1
@@ -90,7 +93,7 @@ Partial Class Form1
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.Location = New System.Drawing.Point(3, 3)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(430, 56)
+        Me.Button1.Size = New System.Drawing.Size(472, 60)
         Me.Button1.TabIndex = 4
         Me.Button1.Text = "Generate DNS" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "or Certificate"
         Me.Button1.UseVisualStyleBackColor = True
@@ -99,11 +102,11 @@ Partial Class Form1
         '
         Me.Button2.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(439, 3)
+        Me.Button2.Location = New System.Drawing.Point(481, 3)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(430, 56)
+        Me.Button2.Size = New System.Drawing.Size(473, 60)
         Me.Button2.TabIndex = 5
-        Me.Button2.Text = "Generate Private Key" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "or Certificate Request"
+        Me.Button2.Text = "Generate TLSA + Key" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "or Certificate Request"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Label1
@@ -130,7 +133,7 @@ Partial Class Form1
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(12, 152)
+        Me.Label3.Location = New System.Drawing.Point(12, 189)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(435, 32)
         Me.Label3.TabIndex = 8
@@ -140,7 +143,7 @@ Partial Class Form1
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(12, 212)
+        Me.Label4.Location = New System.Drawing.Point(12, 255)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(306, 32)
         Me.Label4.TabIndex = 9
@@ -155,19 +158,22 @@ Partial Class Form1
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel1.Controls.Add(Me.Button2, 1, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.Button1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RadioButton1, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.RadioButton2, 1, 1)
         Me.TableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(12, 90)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 1
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(872, 62)
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(957, 96)
         Me.TableLayoutPanel1.TabIndex = 10
         '
         'Button3
         '
         Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button3.Font = New System.Drawing.Font("Segoe MDL2 Assets", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button3.Location = New System.Drawing.Point(793, 215)
+        Me.Button3.Location = New System.Drawing.Point(881, 257)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(92, 31)
         Me.Button3.TabIndex = 11
@@ -178,12 +184,36 @@ Partial Class Form1
         '
         Me.SaveFileDialog1.Filter = "PEM Certificate|*.crt|DER certificate|*.der|PKCS12 File|*.p12"
         '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(3, 69)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(219, 24)
+        Me.RadioButton1.TabIndex = 6
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Use DNS-PERSIST-01"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(481, 69)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(281, 24)
+        Me.RadioButton2.TabIndex = 7
+        Me.RadioButton2.Text = "Use HTTP-01 (with PERSIST)"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CausesValidation = False
-        Me.ClientSize = New System.Drawing.Size(897, 653)
+        Me.ClientSize = New System.Drawing.Size(982, 753)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Controls.Add(Me.Label4)
@@ -195,10 +225,11 @@ Partial Class Form1
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.MinimumSize = New System.Drawing.Size(500, 400)
+        Me.MinimumSize = New System.Drawing.Size(1000, 800)
         Me.Name = "Form1"
         Me.Text = "ACME-nostate"
         Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -217,4 +248,6 @@ Partial Class Form1
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
     Friend WithEvents Button3 As Button
     Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
 End Class
